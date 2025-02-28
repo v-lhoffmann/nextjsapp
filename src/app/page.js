94b6bf12af2fee2eb3cdcbef8c84344f77dc1586
v-lhoffmann <99043209+v-lhoffmann@users.noreply.github.com>
2025-02-27 16:37:44 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            Editing in main branch. <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
